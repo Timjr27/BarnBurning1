bdf95caf4039ec7f3a69c3be2533a5308c1c527f
Timjr27 <57326293+Timjr27@users.noreply.github.com>
2020-01-27 19:49:56 +0100

    Add files via upload

---
+++ Scripts/EventOpener.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventOpener : MonoBehaviour
+{
+
+    private string canvasText = "Default";
+
+    public Canvas canvas;
+    public Transform player;
+    public GameObject hud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableHud()
+    {
+        hud.SetActive(true);
+        
+    }
+
+     public void DisableHud()
+    {
+        if (hud.activeSelf)
+        {
+            Invoke("DHud", 0.5f);
+            
+        }
+        
+    }
+
+    public void DHud()
+    {
+        hud.SetActive(false);
+    }
+
+    public void GetTextInfo(GameObject obj)
+    {
+        Debug.Log(obj.name);
+        
+        if (IsNearEnough(obj))
+        {
+            if (!hud.activeSelf)
+            {
+            Invoke("EnableHud", 0.5f);
+            }
+            canvasText = obj.GetComponent<Text>().text;
+            Debug.Log(obj.name + " sagt: " + canvasText);
+            hud.GetComponentInChildren<Text>().text = canvasText;
+        }
+    }
+
+    private bool IsNearEnough(GameObject objectToCheck)
+    {
+        bool check = false;
+        if (Vector3.Distance(objectToCheck.transform.position,player.position)<5)
+        {
+            check = true;
+        }
+        return check;
+                       
+    }
+
+    private string readText(GameObject gobj)
+    {
+        Debug.Log(gobj.GetComponent<Text>().text);
+
+        return gobj.GetComponent<Text>().text;
+    }
+}
---
+++ Scripts/Lighting.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lighting : MonoBehaviour
+{
+
+    private IEnumerator coroutine;
+
+
+    public Light l1;
+    public GameObject wall;
+    public GameObject lTrigger;
+    public GameObject woman;
+    public GameObject barn;
+    public GameObject physicWoman;
+    public GameObject chairWoman;
+    public GameObject fire;
+
+    private float lightDecrease = 0.1f;
+
+    public AudioSource lighter;
+    public AudioSource expl;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*
+        if (c.isTrigger)
+        if (Input.GetKeyDown("space")){
+            LightingBolt();
+        }*/
+
+        if (Input.GetKeyDown("k") || Input.GetButtonDown("Fire1"))
+        {
+            Explosion();    
+        }
+    }
+
+    public void Explosion()
+    {
+        coroutine = WaitAndExplode();
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndExplode()
+    {
+        lighter.Play();
+        if (Vector3.Distance(lTrigger.transform.position, barn.transform.position) < 15)
+        {
+            yield return new WaitForSeconds(4f);
+            expl.Play();
+            yield return new WaitForSeconds(0.5f);
+
+            fire.SetActive(true);
+            yield return new WaitForSeconds(3.5f);
+
+            barn.SetActive(false);
+            chairWoman.SetActive(true);
+            yield return new WaitForSeconds(1f);
+
+            barn.SetActive(true);
+            chairWoman.SetActive(false);
+            yield return new WaitForSeconds(4f);
+
+            barn.SetActive(false);
+            chairWoman.SetActive(true);
+            yield return new WaitForSeconds(4f);
+
+            barn.SetActive(true);
+            chairWoman.SetActive(false);
+        }
+
+    }
+
+
+    public void LightingBolt()
+    {
+        Debug.Log("BLIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIITZ");
+        coroutine = WaitAndPrint(0.05f);
+        StartCoroutine(coroutine);
+    }
+
+    public void Thunder()
+    {
+        Debug.Log("DONNER");
+        coroutine = WaitAndThunder(0.05f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndThunder(float waitTime)
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 5.0f));
+
+        GetComponent<AudioSource>().Play();
+        physicWoman.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(0.25f);
+        physicWoman.SetActive(false);
+
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+
+        yield return new WaitForSeconds(Random.Range(0.5f, 2.0f));
+
+        l1.intensity = 2;
+        
+        wall.SetActive(true);
+        woman.SetActive(true);
+        yield return new WaitForSeconds(Random.Range(0.01f,0.15f));
+
+        bool boo = true;
+
+        this.GetComponent<AudioSource>().Play();
+        while (boo)
+        {
+            yield return new WaitForSeconds(waitTime);
+            l1.intensity -= lightDecrease;
+            if (l1.intensity < 1.5f)
+            {
+                wall.SetActive(false);
+                lightDecrease = 0.05f;
+                woman.SetActive(false);
+            }
+            if (l1.intensity <= 0.001f)
+            {
+                boo = false;
+                l1.intensity = 0;
+                wall.SetActive(false);
+            }
+            print("WaitAndPrint " + Time.time);
+        }
+
+        
+    }
+
+}
